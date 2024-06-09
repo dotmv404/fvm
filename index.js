d0c36920c970692cdbb8c64653f977ffdf978368
dotmv404 <grasslover045@gmail.com>
2024-06-10 00:51:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -187,9 +187,7 @@ function renderProducts(apiUrl, products, renderProduct) {
 }
 
 // Serve the home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
